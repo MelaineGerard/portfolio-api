12bb164b8b46f4d34dcf19623b5b666dd85ed9c5
Melaine Gérard <pro@melaine-gerard.fr>
2023-08-26 11:52:18 +0200

    :white_check_mark:  Ajout d'un test

---
+++ tests/EntityCreationTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\ProfessionalExperience;
+
+trait EntityCreationTrait
+{
+    public function createProfessionalExperience(
+        string $enterprise = 'Test',
+        string $content = 'Test',
+        string $description = 'Test',
+        string $url = 'https://example.test',
+        string $type = 'Stage',
+        string $startDate = 'Mai 2021',
+        string $endDate = 'Mai 2022',
+
+    ): ProfessionalExperience
+    {
+        $professionalExperience = new ProfessionalExperience();
+
+        $professionalExperience->setEnterprise($enterprise);
+        $professionalExperience->setContent($content);
+        $professionalExperience->setDescription($description);
+        $professionalExperience->setUrl($url);
+        $professionalExperience->setType($type);
+        $professionalExperience->setStartDate($startDate);
+        $professionalExperience->setEndDate($endDate);
+
+        return $professionalExperience;
+    }
+}
